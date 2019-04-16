0d4c48e6d7ef49409579fca1bf04de66adfdcd8a
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-16 15:06:08 +0200

    prezzo minori definito

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ console. log(km);
 var ticket_adulti = km * 0.21 ;
 console. log(ticket_adulti)
 alert('il prezzo standard è' + ticket_adulti + '€');
+var età = prompt ('quanti anni hai?')
+if (età < 14 ) { var prezzo_minori = ticket_adulti * 0.4}
+alert('il prezzo per i minori di 14 anni è' + '' + prezzo_minori + '' + '€')
