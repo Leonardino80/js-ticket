989a0548c0bd1c2a5ef8ba7801bad7114d124bfb
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-16 15:05:49 +0200

    prezzo base definito

---
+++ script.js
@@ -0,0 +1,5 @@
+var km = prompt('quanti kilometri vuoi fare?');
+console. log(km);
+var ticket_adulti = km * 0.21 ;
+console. log(ticket_adulti)
+alert('il prezzo standard è' + ticket_adulti + '€');
