a945e1412f6fec4fd215a409d03770174ff7bd4e
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-16 16:42:04 +0200

    sembra finito... devo controllare

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var km = prompt('quanti kilometri vuoi fare?');
 console. log(km);
 var ticket_adulti = km * 0.21 ;
 console. log(ticket_adulti)
-alert('il prezzo standard è' + ticket_adulti + '€');
-var età = prompt ('quanti anni hai?')
-if (età < 14 ) { var prezzo_minori = ticket_adulti * 0.4}
-alert('il prezzo per i minori di 14 anni è' + '' + prezzo_minori + '' + '€')
+var età = prompt ('quanti anni hai?');
+console. log(età);
+if (età <= 14 ) { var prezzo_minori = ticket_adulti * 0.8;
+alert('il prezzo per i minori di 14 anni è' + ' ' + prezzo_minori + ' ' + '€')};
+if (età >= 65 ) { var prezzo_anziani = ticket_adulti * 0.6;
+alert('il prezzo per gli over 65 è' + ' ' + prezzo_anziani + ' ' + '€');
+}else { alert('il prezzo standard è' + ticket_adulti + ' ' + '€')};
