b23927866a1e90ffc47f0212e7e31684e3cd5b27
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-16 21:11:08 +0200

    arrotondato il prezzo. COnsegna finale. Se riesco faccio il biglietto in flex ma nn credo.

--- script.js
+++ script.js
@@ -4,23 +4,21 @@ var cognome = prompt('Inserisci il tuo cognome..');
 console. log(cognome)
 var km = prompt('quanti kilometri vuoi fare?');
 console. log(km);
-var ticket_adulti = km * 0.21 ;
+var ticket_adulti = Math.floor(km * 0.21 ) ;
 console. log(ticket_adulti);
 var età = prompt ('quanti anni hai?');
 console. log(età);
 if (età < 18 ) {
-  var prezzo_minori = ticket_adulti * 0.8;
+  var prezzo_minori = Math.floor(ticket_adulti * 0.8);
   alert('il prezzo per i minori di 18 anni è' + ' ' + prezzo_minori + ' ' + '€');
   console. log(prezzo_minori);
   document.getElementById('biglietto_emesso').innerHTML = nome + ' ' + cognome + '  Il prezzo del tuo biglietto è' + prezzo_minori + '€';
-}
-else if (età > 64 ) {
-  var prezzo_anziani = ticket_adulti * 0.6;
+} else if (età > 64 ) {
+  var prezzo_anziani = Math.floor(ticket_adulti * 0.6);
   alert('il prezzo per gli over 65 è' + ' ' + prezzo_anziani + ' ' + '€');
   console. log(prezzo_anziani);
   document.getElementById('biglietto_emesso').innerHTML = nome + ' ' + cognome + '  Il prezzo del tuo biglietto è' + prezzo_anziani + '€';
-}
-else {
+} else {
   alert('il prezzo standard è' + ticket_adulti + ' ' + '€');
   document.getElementById('biglietto_emesso').innerHTML = nome + ' ' + cognome + '  Il prezzo del tuo biglietto è' + ticket_adulti + '€';
 }
