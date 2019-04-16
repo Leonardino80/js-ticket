9cad64aa943d2c5666c17fa6ca5e3debeb0e26b2
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-16 17:06:15 +0200

    non riesco a scrivere il risultato nel div!

--- script.js
+++ script.js
@@ -9,3 +9,4 @@ alert('il prezzo per i minori di 14 anni è' + ' ' + prezzo_minori + ' ' + '€'
 if (età >= 65 ) { var prezzo_anziani = ticket_adulti * 0.6;
 alert('il prezzo per gli over 65 è' + ' ' + prezzo_anziani + ' ' + '€');
 }else { alert('il prezzo standard è' + ticket_adulti + ' ' + '€')};
+document.getElementById('biglietto_emesso').innerHTML = 'Il prezzo del tuo biglietto è';
